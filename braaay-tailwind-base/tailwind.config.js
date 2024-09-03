fae524a0f4891210479040115c3d2605065f4dea
minimo-io <nicolas@minimo.io>
2024-09-03 18:50:49 -0300

    starting engines!

--- braaay-tailwind-base/tailwind.config.js
+++ braaay-tailwind-base/tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./src/index.html"],
     theme: {
-        extend: {},
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
+
+        colors: {
+            transparent: "transparent",
+            black: "#000",
+            white: "#fff",
+
+            map: "#293E3F",
+            bra: "#226246",
+            sky: "#226246",
+            sun: "#BD8836",
+            blue: "#212639",
+            uru: "#4C89C8",
+            way: "#B8CADB",
+
+            grey: {
+                background: "#fafafa",
+                "page-primary": "#303239",
+
+                lighter: "#e3e3e3",
+                light: "#c3c3c3",
+                medium: "#a0a0a0",
+                "medium-dark": "#737373",
+                dark: "#303133",
+                darkest: "#1d1d1d",
+            },
+        },
+
+        fontFamily: {
+            prata: ["Prata", "serif"],
+            roboto: ["Roboto", "sans-serif"],
+        },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/aspect-ratio")],
 };
