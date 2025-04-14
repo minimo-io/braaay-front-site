b1b8f6e96766774d2fa9e4c6725c409353bc75fa
Nicolas Erramuspe <nicolas@minimo.io>
2025-04-14 19:57:41 -0300

    Header fix (2)

--- src/lib/components/ui/headers/CategoryHeader.svelte
+++ src/lib/components/ui/headers/CategoryHeader.svelte
@@ -33,7 +33,7 @@
 							/>
 						{/if}
 						<span
-							class="self-center font-roboto font-extrabold align-bottom pt-1 text-left text-[18px] md:text-[22px] truncate max-w-[95%] md:max-w-max"
+							class="self-center font-roboto font-extrabold align-bottom pt-1 text-left text-[18px] md:text-[22px] truncate md:max-w-max max-w-[95%]"
 							>{categoryTitle}</span
 						>
 					</div>
