013e8e83b96ae7383b7ee8005d95bf3bbb0ad1e6
minimo-io <nicolas@minimo.io>
2025-05-16 16:19:32 -0300

    Home products

---
+++ src/lib/data/home-products.data.ts
@@ -0,0 +1,121 @@
+import { m } from '$lib/paraglide/messages';
+
+export const HomeProducts = () => {
+	return {
+		products: [
+			{
+				title: 'Familia Deicas Alvarinho Atlántico Sur 750ml',
+				price: `${m.currencySymbol()}148,00`,
+				url: '/produto/vinho-branco-alvarinho-atlantico-sur-deicas-uruguai/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2025/02/vinho-alvarinho-atlantico-sur-deicas-2.png',
+					alt: 'vinho-alvarinho-atlantico-sur-deicas-2'
+				},
+				discount: '5%'
+			},
+			{
+				title: 'Bouza Albariño 2024',
+				price: `${m.currencySymbol()}285,00`,
+				url: '/produto/vinho-branco-bouza-albarino-2024-uruguai/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2025/05/vinho-albarinho-bouza.png',
+					alt: 'vinho-albarinho-bouza'
+				},
+				discount: ''
+			},
+			{
+				title: 'Família Deicas Massimo Tannat 750ml',
+				price: `${m.currencySymbol()}950,00`,
+				url: '/produto/vinho-familia-deicas-massimo-tannat/',
+				score: '5.0',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/09/vinho-massimo-familia-deicas.png',
+					alt: 'massimo-tannat-familia-deicas'
+				},
+				discount: '20%'
+			},
+			{
+				title: 'Monte Agudo Sublime 750ml',
+				price: `${m.currencySymbol()}127,90`,
+				url: '/produto/vinhedos-do-monte-agudo-sublime/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/08/Sublime-1.webp',
+					alt: 'Sublime (1)'
+				},
+				discount: ''
+			},
+			{
+				title: 'Pizzato Concentus Gran Reserva 750ml',
+				price: `${m.currencySymbol()}190,00`,
+				url: '/produto/vinho-pizzato-concentus-gran-reserva/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/08/PIZZATO-concentus.webp',
+					alt: 'PIZZATO concentus'
+				},
+				discount: ''
+			}
+		],
+		kits: [
+			{
+				title: 'Kit vinho Pinot Noir Premium + Flores naturais',
+				price: `${m.currencySymbol()}390,00`,
+				url: '/produto/kit-vinho-pinot-noir-premium-flores-naturais/',
+				score: '4.8',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/11/kit-vina-eden-flores-naturais-blur.png',
+					alt: 'kit-vina-eden-flores-naturais-blur'
+				},
+				discount: ''
+			},
+			{
+				title: 'Kit x6 espumantes Brut Cume + Sacola',
+				price: `${m.currencySymbol()}299,40`,
+				url: '/produto/kit-x6-espumantes-brut-cume-sacola/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/11/kit-presente-espumante-cume.jpg',
+					alt: 'kit-presente-espumante-cume'
+				},
+				discount: ''
+			},
+			{
+				title: 'Kit x3 Espumantes Especiáis + Taça',
+				price: `${m.currencySymbol()}300,00`,
+				url: '/produto/kit-x3-espumantes-taca/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/11/kit-taca-espumantes-prod.jpg',
+					alt: 'kit-taca-espumantes-prod'
+				},
+				discount: ''
+			},
+			{
+				title:
+					'Kit vinho com quejo: Tannat da Artesana com queijos Brie e Gouda + Pistache para presente',
+				price: `${m.currencySymbol()}249,00`,
+				url: '/produto/kit-vinho-artesana-tannat-queijos-brie-e-gouda-pistache-e-caixa/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/11/kit-artesana-tannat-1.png',
+					alt: 'kit-vinho-e-queijo'
+				},
+				discount: ''
+			},
+			{
+				title: 'Kit Espumante Pizzato Brut Rosé com Chocolates Dengo',
+				price: `${m.currencySymbol()}242,00`,
+				url: '/produto/kit-espumante-pizzato-brut-rose-com-chocolates-dengo/',
+				score: '',
+				image: {
+					src: 'https://braaay.com/wp-content/uploads/2024/11/kit-pizzato-brut-choco-dengo.png',
+					alt: 'kit-espumante-pizzato-chocolate'
+				},
+				discount: ''
+			}
+		]
+	};
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,15 @@
 	import Button from '$components/ui/buttons/Button.svelte';
 	import SectionDivider from '$components/ui/dividers/SectionDivider.svelte';
 	import WineBox from '$components/ui/products/WineBox.svelte';
+	import { HomeProducts } from '$data/home-products.data';
 	import { allCountries } from '$lib/data/countries.data';
 	import { getLocale } from '$lib/paraglide/runtime';
 
 	import { Calendar, ChefHat } from '@lucide/svelte';
 
 	let pageCountries = $state(allCountries[getLocale()]);
+	let homeProducts = HomeProducts();
+	// console.log('HomeProducts', homeProducts);
 </script>
 
 <svelte:head>
@@ -103,73 +106,39 @@
 		<!-- Wine list -->
 		<section class="bg-white max-w-screen-lg-2x py-10 mx-[20px] lg:mx-auto">
 			<div class="bry-product-list">
-				<!-- Wine Box 0 -->
-				<WineBox
-					image={{
-						src: '/images/wines/vinho-don-pascual-tannat-merlot.webp'
-					}}
-					wine={{
-						title: 'Don Pascual Cabernet Franc',
-						price: '150.0',
-						url: '/produto/vinho-familia-deicas-massimo-tannat/',
-						score: '4.5'
-					}}
-				/>
+				{#each homeProducts.products as product, i (i)}
+					<WineBox
+						image={{
+							src: product.image.src
+						}}
+						wine={{
+							title: product.title,
+							price: product.price,
+							url: product.url,
+							score: product.score
+						}}
+						discount={product.discount}
+					/>
+				{/each}
 
-				<!-- Wine Box 1 -->
-				<WineBox
-					image={{
-						src: '/images/wines/Deica-2-1.webp'
-					}}
-					wine={{
-						title: 'Pizzato Merlot de Merlots 2022',
-						price: '150.0',
-						url: '/produto/vinho-familia-deicas-massimo-tannat/',
-						score: '4.5'
-					}}
-				/>
-
-				<!-- Wine Box 2 -->
-				<WineBox
-					image={{
-						src: '/images/wines/PIZZATO-concentus.webp'
-					}}
-					wine={{
-						title: 'Artesana Cabernet Franc/Merlot Reserva',
-						price: '100.0',
-						url: '/produto/vinho-familia-deicas-massimo-tannat/',
-						score: '4.5'
-					}}
-				/>
-
-				<!-- Wine Box 3 -->
-				<WineBox
-					image={{
-						src: '/images/wines/Pizzato-Legno-Chardonnay.webp'
-					}}
-					wine={{
-						title: 'Pizzato Chardonnay de Chardonnays',
-						price: '119.0',
-						url: '/produto/vinho-familia-deicas-massimo-tannat/',
-						score: '4.5'
-					}}
-				/>
-
-				<!-- Wine Box 4 -->
-				<WineBox
-					image={{
-						src: '/images/wines/vinho-don-pascual-cabernet-merlot.webp'
-					}}
-					wine={{
-						title: 'Mayos Jovem Branco da Basso',
-						price: '43.0',
-						url: '/produto/vinho-familia-deicas-massimo-tannat/',
-						score: '4.5'
-					}}
-				/>
+				<!-- Kits -->
+				{#each homeProducts.kits as product, i (i)}
+					<WineBox
+						image={{
+							src: product.image.src
+						}}
+						wine={{
+							title: product.title,
+							price: product.price,
+							url: product.url,
+							score: product.score
+						}}
+						discount={product.discount}
+					/>
+				{/each}
 
 				<!-- Wine Box 6 (with discount) -->
-				<WineBox
+				<!-- <WineBox
 					image={{
 						src: '/images/wines/vinho-mayos-jovem-branco.webp',
 						alt: 'Artesana Cabernet Franc/Merlot Reserva'
@@ -181,7 +150,7 @@
 						score: '4.5'
 					}}
 					discount={'50%'}
-				/>
+				/> -->
 			</div>
 		</section>
 
