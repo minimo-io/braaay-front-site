f585f5d7d9c9501aba6e2c5872c16bad2be02010
Nicolas Erramuspe <nicolas@minimo.io>
2025-08-06 13:47:17 -0300

    uy carousel started

--- src/lib/components/ui/Carrousel.svelte
+++ src/lib/components/ui/Carrousel.svelte
@@ -2,7 +2,9 @@
 	import { onMount } from 'svelte';
 	import Glide from '@glidejs/glide';
 	import '@glidejs/glide/dist/css/glide.core.css';
-	import { localizeHref } from '$lib/paraglide/runtime';
+	import { getLocale, localizeHref } from '$lib/paraglide/runtime';
+
+	const locale = $derived(getLocale());
 
 	onMount(async () => {
 		let glide = new Glide('#glide-carousel', {
@@ -21,81 +23,112 @@
 <div id="glide-carousel" class="glide">
 	<div class="glide__track" data-glide-el="track">
 		<ul class="glide__slides">
-			<li class="glide__slide w-full">
-				<a
-					href={localizeHref('/produto/confraria-entre-tacas-e-mulheres/')}
-					class="not-hover-effect"
-				>
-					<picture>
-						<!-- Mobile image: for screens up to 767px -->
-						<source
-							srcset="/images/carousel/braaay-carousel-mobile-1.jpg"
-							media="(max-width: 767px)"
-						/>
-						<!-- Desktop image: for screens 768px and above -->
-						<source
-							srcset="/images/carousel/braaay-carousel-desktop-1.jpg"
-							media="(min-width: 768px)"
-						/>
-						<!-- Fallback image -->
-						<img
-							src="/images/carousel/braaay-carousel-desktop-1.jpg"
-							alt="placeholder 2"
-							class="carousel-image w-full"
-							width="800"
-							height="600"
-						/>
-					</picture>
-				</a>
-			</li>
-			<li class="glide__slide w-full">
-				<a href={localizeHref('/kits/')} class="not-hover-effect">
-					<picture>
-						<!-- Mobile image: for screens up to 767px -->
-						<source
-							srcset="/images/carousel/braaay-carousel-mobile-2.jpg"
-							media="(max-width: 767px)"
-						/>
-						<!-- Desktop image: for screens 768px and above -->
-						<source
-							srcset="/images/carousel/braaay-carousel-desktop-2.jpg"
-							media="(min-width: 768px)"
-						/>
-						<!-- Fallback image -->
-						<img
-							src="/images/carousel/braaay-carousel-desktop-2.jpg"
-							alt="placeholder 3"
-							class="carousel-image w-full"
-							width="800"
-							height="600"
-						/>
-					</picture>
-				</a>
-			</li>
-			<li class="glide__slide w-full">
-				<a href="./" class="not-hover-effect">
-					<picture>
-						<!-- Mobile image: for screens up to 767px -->
-						<source
-							srcset="/images/carousel/frete-gratis-2025-mobile.png"
-							media="(max-width: 767px)"
-						/>
-						<!-- Desktop image: for screens 768px and above -->
-						<source
-							srcset="/images/carousel/frete-gratis-2025-desktop.png"
-							media="(min-width: 768px)"
-						/>
-						<!-- Fallback image -->
-						<img
-							src="/images/carousel/frete-gratis-2025-desktop.png"
-							alt="placeholder 2"
-							class="carousel-image w-full"
-							width="800"
-							height="600"
-						/>
-					</picture>
-				</a>
-			</li>
+			{#if locale == 'pt'}
+				<li class="glide__slide w-full">
+					<a
+						href={localizeHref('/produto/confraria-entre-tacas-e-mulheres/')}
+						class="not-hover-effect"
+					>
+						<picture>
+							<!-- Mobile image: for screens up to 767px -->
+							<source
+								srcset="/images/carousel/braaay-carousel-mobile-1.jpg"
+								media="(max-width: 767px)"
+							/>
+							<!-- Desktop image: for screens 768px and above -->
+							<source
+								srcset="/images/carousel/braaay-carousel-desktop-1.jpg"
+								media="(min-width: 768px)"
+							/>
+							<!-- Fallback image -->
+							<img
+								src="/images/carousel/braaay-carousel-desktop-1.jpg"
+								alt="placeholder 2"
+								class="carousel-image w-full"
+								width="800"
+								height="600"
+							/>
+						</picture>
+					</a>
+				</li>
+				<li class="glide__slide w-full">
+					<a href={localizeHref('/kits/')} class="not-hover-effect">
+						<picture>
+							<!-- Mobile image: for screens up to 767px -->
+							<source
+								srcset="/images/carousel/braaay-carousel-mobile-2.jpg"
+								media="(max-width: 767px)"
+							/>
+							<!-- Desktop image: for screens 768px and above -->
+							<source
+								srcset="/images/carousel/braaay-carousel-desktop-2.jpg"
+								media="(min-width: 768px)"
+							/>
+							<!-- Fallback image -->
+							<img
+								src="/images/carousel/braaay-carousel-desktop-2.jpg"
+								alt="placeholder 3"
+								class="carousel-image w-full"
+								width="800"
+								height="600"
+							/>
+						</picture>
+					</a>
+				</li>
+				<li class="glide__slide w-full">
+					<a href="./" class="not-hover-effect">
+						<picture>
+							<!-- Mobile image: for screens up to 767px -->
+							<source
+								srcset="/images/carousel/frete-gratis-2025-mobile.png"
+								media="(max-width: 767px)"
+							/>
+							<!-- Desktop image: for screens 768px and above -->
+							<source
+								srcset="/images/carousel/frete-gratis-2025-desktop.png"
+								media="(min-width: 768px)"
+							/>
+							<!-- Fallback image -->
+							<img
+								src="/images/carousel/frete-gratis-2025-desktop.png"
+								alt="placeholder 2"
+								class="carousel-image w-full"
+								width="800"
+								height="600"
+							/>
+						</picture>
+					</a>
+				</li>
+			{:else if locale == 'uy'}
+				<!-- -->
+				<li class="glide__slide w-full">
+					<a
+						href={localizeHref('/produto/confraria-entre-tacas-e-mulheres/')}
+						class="not-hover-effect"
+					>
+						<picture>
+							<!-- Mobile image: for screens up to 767px -->
+							<source
+								srcset="/images/carousel/braaay-carousel-mobile-1.jpg"
+								media="(max-width: 767px)"
+							/>
+							<!-- Desktop image: for screens 768px and above -->
+							<source
+								srcset="/images/carousel/braaay-carousel-desktop-uy-1.jpg"
+								media="(min-width: 768px)"
+							/>
+							<!-- Fallback image -->
+							<img
+								src="/images/carousel/braaay-carousel-desktop-uy-1.jpg"
+								alt="carousel-uy-1"
+								class="carousel-image w-full"
+								width="800"
+								height="600"
+							/>
+						</picture>
+					</a>
+				</li>
+			{/if}
 
 			<!-- Add more slides as needed -->
 		</ul>
