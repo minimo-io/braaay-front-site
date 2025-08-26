44501a4f37c9cb131af30d929201cd6d39b5d583
minimo-io <nicolas@minimo.io>
2025-08-26 11:22:55 -0300

    187ml added

--- src/lib/data/bottle-sizes.data.ts
+++ src/lib/data/bottle-sizes.data.ts
@@ -1,6 +1,6 @@
 // import type { Grapes } from '$lib/types';
 
 export const bottleSizes = {
-	pt: ['750ml', '500ml', '473ml-lata', '375ml', '250ml'],
+	pt: ['750ml', '500ml', '473ml-lata', '375ml', '250ml', '187ml'],
 	uy: []
 };
